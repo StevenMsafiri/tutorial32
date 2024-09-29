5f33f2b2797829b6df98bc7246b42b5cee489c07
StevenMsafiri <ianleonardmk47@gmail.com>
2024-09-29 20:47:53 +0300

    second commit

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,10 +1,22 @@
-import smtplib
-my_email = "appbreweryinfo@gmail.com"
-password = "ildqyaojtomcgauu"
+# import smtplib
+# my_email = "ianleonardmk47@gmail.com"
+# password = "vbwtuxcedgdtfozi"
+#
+#
+# with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email, to_addrs="mandegemasebu@yahoo.com", msg="Subject:Greetings \n"
+#                                                                                 "\nHello")
+## connection.close()
 
+import datetime as dt
 
-connection = smtplib.SMTP('smtp.gmail.com', 587)
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="appbrewerytesting@yahoo.com", msg="Hello")
-connection.close()
+from dateutil.rrule import weekday
+
+now = dt.datetime.now()
+year = now.year
+week_day = now.weekday()
+
+date_of_birth = dt.date(year = 2002, month=2, day=5)
+print(date_of_birth)
